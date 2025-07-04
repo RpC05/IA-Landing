665b0e5223bf0494d62287c6db2591bd88edc96a
RpC05 <jpalominoc5@upao.edu.pe>
2025-07-03 23:43:43 -0500

    feat: adding icon and tittle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'AcademicProfessor',
+  description: 'IA project to help students with their academic needs',
   generator: 'v0.dev',
 }
 
